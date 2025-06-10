8d43124e6e5ba0dcf5d8480c15e8d273d7fa8564
Gh0sty23 <maxxyisagh0st@gmail.com>
2025-06-10 18:24:54 +0800

    igamer

--- virtual-backpack/vite.config.ts
+++ virtual-backpack/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://Gh0sty23.github.io/Virtual-backpack"
+  base: "/virtual-backpack-site
 })
